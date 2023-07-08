6d28c913a24118f199181bc17c9098b063ee6a3e
wqj <wqjuser@gmail.com>
2023-07-09 02:56:26 +0800

    将更新type换回date

--- app/store/update.ts
+++ app/store/update.ts
@@ -58,7 +58,7 @@ async function getVersion(type: "date" | "tag") {
 export const useUpdateStore = create<UpdateStore>()(
   persist(
     (set, get) => ({
-      versionType: "tag",
+      versionType: "date",
       lastUpdate: 0,
       version: "unknown",
       remoteVersion: "",
