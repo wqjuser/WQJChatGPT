7d84f5282605d5b862fcb7e0e08fe67c17a96a50
wqj <wqjuser@gmail.com>
2023-07-10 12:06:10 +0800

    尝试修改返回值

--- app/store/chat.ts
+++ app/store/chat.ts
@@ -333,11 +333,17 @@ export const useChatStore = create<ChatStore>()(
           },
           onError(error) {
             const isAborted = error.message.includes("aborted");
+            let message = error.message;
+            if (
+              message.includes("余额不足如果您需要一个访问码,可以前往此处购买:")
+            ) {
+              message = "余额不足,请联系开发者";
+            }
             botMessage.content =
               "\n\n" +
               prettyObject({
                 error: true,
-                message: error.message,
+                message: message,
               });
             botMessage.streaming = false;
             userMessage.isError = !isAborted;
